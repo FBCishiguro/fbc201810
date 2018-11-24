9fa4b65b236731cb1c0e32aa31643dac1476271c
FBCishiguro <37077515+FBCishiguro@users.noreply.github.com>
2018-11-24 18:48:57 +0100

    20181124 cash

--- wp-content/themes/wsc7/searchform.php
+++
@@ -1,8 +0,0 @@
-<?php /* WordPress CMS Theme WSC Project. */ ?>
-
-<form role="search" method="get" class="searchform" action="<?php bloginfo('url'); ?>" >
-<input type="text" value="" name="s" class="s searchbox"  value="<?php the_search_query(); ?>" />
-<input type="submit" class="searchsubmit" value="検索" />
-</form>
-
-
